9e39b8b92c90905991d2cbb9a7ef13f00054e02d
Sebastian Locksenius <slocksenius@gmail.com>
2023-03-25 00:35:04 +0100

    lade till kommentarer i errandcontroller så att alla vet hur man ska testa funktionerna

--- controllers/errandController.js
+++ controllers/errandController.js
@@ -3,10 +3,10 @@ const errandModel = require('../models/errandModel')
 
 const router = require('express').Router()
 
-router.post('/', errandModel.createNewErrand)
+router.post('/', errandModel.createNewErrand)   //POST på errands/ med JSON "email": "x", "subject": "x","message": "x"
 
-router.get('/', errandModel.getAllErrands)
+router.get('/', errandModel.getAllErrands)  //GET på errands/
 
-router.put('/:id', errandModel.updateErrand)
+router.put('/:id', errandModel.updateErrand) // PUT på errands/id med JSON: "status": 1/2/3
 
 module.exports = router

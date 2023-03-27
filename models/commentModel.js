5a488346e9f21fffd507e5b016c13bc3d027f4fc
Peter Hogler <me@peterhogler.dev>
2023-03-27 20:19:08 +0200

    added how to add comments to errand

--- models/commentModel.js
+++ models/commentModel.js
@@ -20,39 +20,42 @@ exports.createNewComment = (req, res) => {
         return res.status(400).json({ message: "Fields cannot be empty" });
     }
 
-   // Errand.findById(caseId)   //Allt här som är bortkommenterat är för att få populate att fungera någonlunda
-     //   .then(() => {
-            Comment.create({ caseId, email, message })
-                .then((data) => {
-                    res.status(201).json(data);
-                })
-                .catch((error) => {
-                    res.status(400).json({ message: "Unable to create case" });
+    // Errand.findById(caseId)   //Allt här som är bortkommenterat är för att få populate att fungera någonlunda
+    //   .then(() => {
+    Comment.create({ caseId, email, message })
+        .then((newComment) => {
+            Errand.findById(caseId).then((caseToUpdate) => {
+                caseToUpdate.comment.push(newComment._id);
+                caseToUpdate.save().then(() => {
+                    res.status(200).json(caseToUpdate)
                 });
-//        })
-        // .catch((error) => {
-        //     res.status(400).json({
-        //         message: "Case does not exist in errand collection",
-        //     });
-        // });
+            });
+        })
+        .catch((error) => {
+            res.status(400).json({ message: "Unable to create case" });
+        });
+    //        })
+    // .catch((error) => {
+    //     res.status(400).json({
+    //         message: "Case does not exist in errand collection",
+    //     });
+    // });
 };
 exports.deleteComment = (req, res) => {
-
     Comment.findByIdAndDelete(req.params.id)
-    .then(data => {
-        if(!data) {
-            res.status(404).json({
-                message: 'Could not find that Comment'
-            })
-        }
+        .then((data) => {
+            if (!data) {
+                res.status(404).json({
+                    message: "Could not find that Comment",
+                });
+            }
 
-        res.status(200).json({ id: data._id })
-    })
-    .catch(err => {
-        res.status(500).json({
-            message: 'Something went wrong when deleting the Comment',
-            err: err.message
+            res.status(200).json({ id: data._id });
         })
-    })
-
-}
+        .catch((err) => {
+            res.status(500).json({
+                message: "Something went wrong when deleting the Comment",
+                err: err.message,
+            });
+        });
+};
--- schemas/errandSchema.js
+++ schemas/errandSchema.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const errandSchema = mongoose.Schema({
-    email: { type: String, required: true },
-    subject: { type: String, required: true },
-    message: { type: String, required: true },
-    status: { type: Number, default: 1 },
-    comment: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' },
+const errandSchema = mongoose.Schema(
+    {
+        email: { type: String, required: true },
+        subject: { type: String, required: true },
+        message: { type: String, required: true },
+        status: { type: Number, default: 1 },
+        comment: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
+    },
+    { timestamps: true }
+);
 
-}, { timestamps: true })
-
-module.exports = mongoose.model('Errand', errandSchema)
+module.exports = mongoose.model("Errand", errandSchema);
